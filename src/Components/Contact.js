a74e29a4617f8707fdcb4070ac92ed9b7d519821
Melyn Atieno Were <melynatieno@gmail.com>
2022-12-21 12:57:11 +0300

    add icons

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "../Styles/Contact.css"
 import { FaEnvelope, FaGithub, FaLinkedin, FaPhoneAlt } from "react-icons/fa";
 import { MdInfo } from "react-icons/md";
 import { HiLocationMarker } from "react-icons/hi";
@@ -6,12 +7,14 @@ import { HiLocationMarker } from "react-icons/hi";
 function Contact() {
   return (
     <div className='contact'>
-        <h3 className='contact-me'><b>CONTACT INFORMATION <MdInfo /></b></h3> 
-        <h5 className='tel'><FaPhoneAlt /> +254 700 150 645 </h5>
-        <h5 className='email'><FaEnvelope /> melynatieno@gmail.com </h5>
+      <h3 className='contact-me'><b>CONTACT INFORMATION <MdInfo /></b></h3>
+      <h5 className='tel'><FaPhoneAlt /> +254 700 150 645 </h5>
+      <a href="mailto:melynatieno@gmail.com" className='email'><h5><FaEnvelope /> melynatieno@gmail.com </h5></a>
       <h5 className='location'><HiLocationMarker />Nairobi, Kenya</h5>
-        <FaGithub />
-      <FaLinkedin />
+      <div className='socials'>
+      <a href='https://github.com/MelynAtieno'> <FaGithub /></a>
+      <a href='https://www.linkedin.com/in/melyn-atieno/'><FaLinkedin />  </a>
+      </div>
 
     </div>
   )
