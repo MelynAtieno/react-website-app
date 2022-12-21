7cd4aee3b1a336fbf51e36c482ec4926e1096a56
Melyn Atieno Were <melynatieno@gmail.com>
2022-12-21 14:33:35 +0300

    style the contact component

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import "../Styles/Contact.css"
 import { FaEnvelope, FaGithub, FaLinkedin, FaPhoneAlt } from "react-icons/fa";
-import { MdInfo } from "react-icons/md";
+
 import { HiLocationMarker } from "react-icons/hi";
 
 function Contact() {
   return (
     <div className='contact'>
-      <h3 className='contact-me'><b>CONTACT INFORMATION <MdInfo /></b></h3>
+      <h2 className='contact-me'><b>CONTACT ME:</b></h2>
       <h5 className='tel'><FaPhoneAlt /> +254 700 150 645 </h5>
       <a href="mailto:melynatieno@gmail.com" className='email'><h5><FaEnvelope /> melynatieno@gmail.com </h5></a>
       <h5 className='location'><HiLocationMarker />Nairobi, Kenya</h5>
       <div className='socials'>
-      <a href='https://github.com/MelynAtieno'> <FaGithub /></a>
-      <a href='https://www.linkedin.com/in/melyn-atieno/'><FaLinkedin />  </a>
+      <a href='https://github.com/MelynAtieno' target="_blank" rel='noopener noreferrer' className='github'> <FaGithub size={50} /></a>
+      <a href='https://www.linkedin.com/in/melyn-atieno/'  target="_blank" rel='noopener noreferrer' className='linkedin'><FaLinkedin size={50} />  </a>
       </div>
 
     </div>
