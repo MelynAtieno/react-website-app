0f4ad457df7ff93aae0007d4674b2f1e0482674c
Melyn Atieno Were <melynatieno@gmail.com>
2022-12-21 12:05:28 +0300

    Add about,contact,projects, resume components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
 
 function App() {
   return (
     <div className="App">
+       <Navbar />
       <Home />
-      <Navbar />
+
+      <Routes>
+        <Route path="/home" element={<Home />}> </Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/resume" element={<Resume />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/contact" element={ <Contact />}></Route>
+      </Routes>
+     
     </div>
   );
 }
