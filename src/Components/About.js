9e6370a28800096f37d345710aae2c1e24efa1bd
Melyn Atieno Were <melynatieno@gmail.com>
2023-01-03 20:18:52 +0300

    edit about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <h5 className='about'> I am self-driven, creative and very passionate about using technology to solve problems. </h5>
+      <h2 className='journey'>MY JOURNEY</h2>
+      
+
+    </div>
   )
 }
 
