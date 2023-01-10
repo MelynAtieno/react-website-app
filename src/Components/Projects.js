6bd0655057919d3d2a5bd7885ccfc42a2243520a
Melyn Atieno Were <melynatieno@gmail.com>
2023-01-10 13:01:51 +0300

    add box shadow to project images

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,15 +2,16 @@ import React from 'react'
 import todo from "../Images/todo-app.png"
 import weather from "../Images/weather-app.png"
 import movie from "../Images/movie-app.png"
+import "../Styles/Projects.css"
 
 function Projects() {
   return (
     <div className='projects'>
         <h2 className='look'><b>TAKE A LOOK!</b></h2>
         <div className='project-images'>
-        <img src={todo} alt="todo-app" width={900} height={400}></img>
-        <img src={weather} alt="weather-app" width={900} height={400}></img>
-        <img src={movie} alt="movie-app" width={900} height={400}></img>
+        <a href='https://frangollo-to-do-app.netlify.app/' target="_blank" rel="noreferrer"><img src={todo} alt="todo-app" width={900} height={400}></img> </a> <br/> <br/>
+        <a href='https://melynatieno.github.io/weather-app/' target="_blank" rel="noreferrer" ><img src={weather} alt="weather-app" width={900} height={400}></img> </a><br/><br/>
+        <a href='https://starlit-movie-app-1c6997.netlify.app/' target="_blank" rel="noreferrer"><img src={movie} alt="movie-app" width={900} height={400}></img> </a>
         </div>
     </div>
   )
