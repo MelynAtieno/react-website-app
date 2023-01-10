944a598fae5726418fc72ecbf99631203cf9d0bf
Melyn Atieno Were <melynatieno@gmail.com>
2023-01-10 12:39:08 +0300

    add project images on project page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import todo from "../Images/todo-app.png"
+import weather from "../Images/weather-app.png"
+import movie from "../Images/movie-app.png"
 
 function Projects() {
   return (
     <div className='projects'>
         <h2 className='look'><b>TAKE A LOOK!</b></h2>
+        <div className='project-images'>
+        <img src={todo} alt="todo-app" width={900} height={400}></img>
+        <img src={weather} alt="weather-app" width={900} height={400}></img>
+        <img src={movie} alt="movie-app" width={900} height={400}></img>
+        </div>
     </div>
   )
 }
