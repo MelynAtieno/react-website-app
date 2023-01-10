ac5672b34774bd54be876bcad6e3cf84a7d70949
Melyn Atieno Were <melynatieno@gmail.com>
2023-01-10 17:07:48 +0300

    add poverty line screenshot

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,6 +2,8 @@ import React from 'react'
 import todo from "../Images/todo-app.png"
 import weather from "../Images/weather-app.png"
 import movie from "../Images/movie-app.png"
+import clinic from "../Images/clinic.jpg"
+import poverty from "../Images/poverty.png"
 import "../Styles/Projects.css"
 
 function Projects() {
@@ -20,6 +22,21 @@ function Projects() {
         <a href='https://starlit-movie-app-1c6997.netlify.app/' target="_blank" rel="noreferrer"><img src={movie} alt="movie-app" width={900} height={400}></img> </a>
         <h4 className='languages'><b>Used: HTML5, CSS3, ReactJS</b></h4>
         <button className='source-code'><a href='https://github.com/MelynAtieno/movie-app' target="_blank" rel="noreferrer"><b>SOURCE CODE</b></a></button> <br/> <br/>
+        
+        
+        <a href='https://github.com/Mrs-Awesome-Master-Neat/online-clinic' target="_blank" rel="noreferrer"><img src={clinic} alt="clinic-app" width={900} height={400}></img> </a>
+        <h3><b>ONLINE CLINIC</b></h3>
+        <h4><b>Allows patients to seek medical advice from medical professionals online.</b></h4>
+        <h4 className='languages'><b>Used: HTML5, CSS3, ReactJS, Ruby on Rails</b></h4>
+
+        <a href='https://github.com/MelynAtieno/Poverty_Line_client' target="_blank" rel="noreferrer"><img src={poverty} alt="poverty-app" width={900} height={400}></img> </a>
+        <h3><b>POVERTY-LINE</b></h3>
+        <h4><b>Helps to reduce poverty by generating employment opportunities 
+          for the less fortunate people and collecting donations to help those in need.
+</b></h4>
+        <h4 className='languages'><b>Used: HTML5, CSS3, ReactJS, Ruby on Rails, PostgreSQL</b></h4>
+
+        
         
         </div>
         
