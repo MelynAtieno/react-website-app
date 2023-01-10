101bf5e11ec493dfc25f18e9887b0e98d4687bf6
Melyn Atieno Were <melynatieno@gmail.com>
2023-01-10 16:08:40 +0300

    edit about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,8 +4,7 @@ import "../Styles/About.css"
 function About() {
   return (
     <div className='abt-page'>
-      <p className='about'> My name is <b>Melyn Atieno Were</b>
-        . Currently, I hold a Bachelor's Degree in Financial Economics but I have always had a passion for tech
+      <p className='about'> I hold a Bachelor's Degree in Financial Economics but I have always had a passion for tech
         and this made me join a Bootcamp to learn Software Engineering. I enjoyed the experience
         and I look forward to progressing in my career as a Software Engineer. <br/> <br/>
         I really enjoy solving complex problems and I love how I can use technology to solve problems. I 
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -9,7 +9,7 @@ function Projects() {
     <div className='projects'>
         <h2 className='look'><b>TAKE A LOOK!</b></h2>
         <div className='project-images'>
-        <a href='https://frangollo-to-do-app.netlify.app/' target="_blank" rel="noreferrer"><img src={todo} alt="todo-app" width={900} height={400}></img> </a> <br/> <br/>
+        <a href='https://frangollo-to-do-app.netlify.app/' target="_blank" rel="noreferrer"><img src={todo} alt="todo-app" width={900} height={400}></img></a> <br/> <br/>
         <a href='https://melynatieno.github.io/weather-app/' target="_blank" rel="noreferrer" ><img src={weather} alt="weather-app" width={900} height={400}></img> </a><br/><br/>
         <a href='https://starlit-movie-app-1c6997.netlify.app/' target="_blank" rel="noreferrer"><img src={movie} alt="movie-app" width={900} height={400}></img> </a>
         
